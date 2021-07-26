dda19e9552e74112ab1504ba3fcb573c929669d4
Red3ffect <red3ffect117@gmail.com>
2021-07-25 21:36:06 -0700

    Adds Notes Crud Routes and connects front end with back end

--- routes/apiRoutes/note-routes.js
+++ routes/apiRoutes/note-routes.js
@@ -1,1 +1,29 @@
 const router = require('express').Router();
+const { findById, createNewNote, deleteNote } = require('../../lib/notes');
+const { notes } = require('../../db/db.json');
+
+// get all notes
+router.get('/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+});
+
+router.get('/notes/:id', (req, res) => {
+    let result = findById(req.params.id, notes);
+    res.json(result);
+});
+
+// create new Note
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    let result = deleteNote(req.params.id, notes);
+    res.json(result);
+});
+
+module.exports = router;
