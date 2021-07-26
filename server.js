c5856c5196f01043fd970e5f48aaea0c54016901
Red3ffect <red3ffect117@gmail.com>
2021-07-25 18:35:16 -0700

    Adds server.js and simple html route

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+// app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+const { db } = require('./db/db.json');
+
+app.listen(PORT, () => {
+    console.log(`Now listening on port ${PORT}`);
+});
